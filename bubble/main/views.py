fa9caadcdfb81b1d114698e0177e7b7f7016b598
James Grantham <granthamjames@yahoo.com>
2023-05-06 11:16:20 -0600

    fixed setting.html submit issues

--- bubble/main/views.py
+++ bubble/main/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Profile
-import logging
 
-logger = logging.getLogger(__name__)
-logger.debug('This is a debug message') #only for testing delete after
 
 # Create your views here.
 
@@ -15,10 +11,11 @@ logger.debug('This is a debug message') #only for testing delete after
 def index(request):
     return render(request, 'index.html')
 
+
 @login_required(login_url='signin')
 def settings(request):
     user_profile = Profile.objects.get(user=request.user)
-    
+    print(request.method)
     if request.method == 'POST':
 
         if request.FILES.get('image') == None:
@@ -40,12 +37,11 @@ def settings(request):
             user_profile.bio = bio
             user_profile.location = location
             user_profile.save()
-            
+
             return redirect('settings')
 
-
-
     return render(request, 'setting.html', {'user_profile': user_profile})
+
 
 def signup(request):
 
@@ -54,7 +50,7 @@ def signup(request):
         email = request.POST['email']
         password = request.POST['password']
         password2 = request.POST['password2']
-    
+
         if password == password2:
             if User.objects.filter(email=email).exists():
                 messages.info(request, 'Email Taken')
@@ -63,27 +59,30 @@ def signup(request):
                 messages.info(request, 'Username Taken')
                 return redirect('signup')
             else:
-                user = User.objects.create_user(username=username, email=email, password=password) 
-                user.save()   
+                user = User.objects.create_user(
+                    username=username, email=email, password=password)
+                user.save()
 
-                #log user in and redirect to settings page 
-                user_login = auth.authenticate(username=username, password=password)
+                # log user in and redirect to settings page
+                user_login = auth.authenticate(
+                    username=username, password=password)
                 auth.login(request, user_login)
 
-                #create a Profile object for the new user
+                # create a Profile object for the new user
                 user_model = User.objects.get(username=username)
-                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile = Profile.objects.create(
+                    user=user_model, id_user=user_model.id)
                 new_profile.save()
                 return redirect('settings')
         else:
             messages.info(request, 'Password Not Matching')
-            return redirect ('signup')
+            return redirect('signup')
 
     else:
         return render(request, 'signup.html')
 
-def signin(request):
 
+def signin(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -91,14 +90,15 @@ def signin(request):
         user = auth.authenticate(username=username, password=password)
 
         if user is not None:
-            auth.login(request,user)
+            auth.login(request, user)
             return redirect('/')
         else:
             messages.info(request, 'credentials invalid')
             return redirect('signin')
 
-    else:    
+    else:
         return render(request, 'signin.html')
+
 
 @login_required(login_url='signin')
 def logout(request):
